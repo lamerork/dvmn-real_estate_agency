7c3c275e82ddc3c5628a934195fcf7633e70427e
Anton Ozerov <lamerork@gmail.com>
2023-08-23 15:39:34 +0300

    Setup admin UI for likes

--- property/admin.py
+++ property/admin.py
@@ -10,7 +10,9 @@ class FlatAdmin(admin.ModelAdmin):
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
     search_fields = ('town',)
     readonly_fields = ('created_at',)
+    raw_id_fields = ('likes',)
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ('user', 'flat',)
     raw_id_fields = ('user', 'flat',)
